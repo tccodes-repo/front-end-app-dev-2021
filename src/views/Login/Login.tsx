e3dd7b8ce99410c1c5f3f82abc33b5ef9b4ac9c6
Justin <itsme@justingauthier.com>
2021-04-27 23:16:55 -0400

    Google auth stubbed in and working

--- src/views/Login/Login.tsx
+++ src/views/Login/Login.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useLocation } from 'react-router-dom';
 import { AuthFrame } from 'components/AuthFrame';
 import { Panel } from 'components/Panel';
@@ -9,21 +9,36 @@ export const Login: React.FC = props => {
 	const location: any = useLocation();
 	const { setCurrentUser } = useAuth();
 
-	const loginUser = () => {
-		console.log();
-		setCurrentUser(
-			{
-				currentUserIdToken: 'test'
-			},
-			location?.state?.from?.pathname
-		);
-	};
+	useEffect(() => {
+		// @ts-expect-error
+		const { gapi } = window;
+
+		gapi.load('auth2', () => {
+			const auth2 = gapi.auth2.init({
+				client_id: '671342783435-1u2km52augr7kglbqc3jbap72tckv0d5.apps.googleusercontent.com',
+				cookiepolicy: 'single_host_origin'
+			});
+
+			auth2.attachClickHandler(document.getElementById('customBtn'), {}, (googleUser: any) => {
+				const profile = googleUser.getBasicProfile();
+				const idToken = googleUser.getAuthResponse().id_token;
+				setCurrentUser(
+					{
+						currentUserEmail: profile.getEmail(),
+						currentUserName: profile.getName(),
+						currentUserIdToken: idToken
+					},
+					location?.state?.from?.pathname
+				);
+			});
+		});
+	}, []);
 
 	return (
 		<AuthFrame>
 			<Panel title='Login' headingLevel='h1'>
 				<p>Use any Google account to login</p>
-				<Button buttonType={'success'} onClick={loginUser}>
+				<Button buttonType={'success'} id='customBtn'>
 					Login with Google
 				</Button>
 			</Panel>
